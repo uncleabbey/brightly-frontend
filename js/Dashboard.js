0aa11f6152f1e1f4f8bc1bf5d786e6538f4cfe54
Kayode Gabriel Abiodun <kayodegabriela@gmail.com>
2020-12-10 11:23:46 +0100

    add new fixes

--- js/Dashboard.js
+++ js/Dashboard.js
@@ -7,6 +7,26 @@ const today = new Date();
 dateElement.innerHTML= today.toLocaleDateString("en-US", options);
 anotherDateElement.innerHTML= today.toLocaleDateString("en-US", options);
 
+const loadData = async () => {
+    const url = "https://brightly-api.herokuapp.com/api/v1/auth/me";
+    
+    try {
+        const res = await makeFetch(url)
+        console.log(res.data)
+        if(res.status === "success") {
+            displayAlert("success", res.message)
+            const first = document.querySelector(".firstName");
+            const { firstName} = res.data.user;
+            first.innerHTML = firstName;
+        } else {
+          displayAlert("danger", res.error);
+          location.href = "/login.html"
+        } 
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 // initialize load data
 loadData()
